a68c531492936655192d0c20c2e27d4c025a961f
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-05 00:40:07 +0200

    [UPD] Add comment and fix code OddEven

--- js/script.js
+++ js/script.js
@@ -1,57 +1,52 @@
 let pronostic;
+let oddEven;
 
+// Chiedi il pronostico all'utente finché non inserisce un valore valido
 do {
     pronostic = prompt("Inserisci il pronostico (pari o dispari):").toLowerCase();
     if (pronostic !== "pari" && pronostic !== "dispari") {
-        alert('Il prompt deve essere "pari" o "dispari"');
+        alert('Il pronostico deve essere "pari" o "dispari"');
     }
 }
 while (pronostic !== "pari" && pronostic !== "dispari");
 
 let userRoll;
 
+// Chiedi il numero del dado all'utente finché non inserisce un valore valido
 do {
-    userRoll = prompt("Inserisci il numero del dado (da 1 a 6):");
+    userRoll = parseInt(prompt("Inserisci il numero del dado (da 1 a 6):"));
     if (isNaN(userRoll) || userRoll < 1 || userRoll > 6) {
-        alert('Il prompt deve essere un numero deve essere da 1 a 6');
+        alert('Il numero deve essere compreso tra 1 e 6');
     }
 }
 while (isNaN(userRoll) || userRoll < 1 || userRoll > 6);
 
+// Genera numero pc con funzione e stampa
 let computerRoll = randomDiceRoll();
+alert(`Il computer ha giocato ${computerRoll}`);
 
-let diceSum = parseInt(userRoll) + parseInt(computerRoll);
+// Somma risultati dadi e controlla pari e dispari
+let diceSum = userRoll + computerRoll;
+oddEven = checkEvenOdd(diceSum);
 
-checkEvenOdd(diceSum);
 
+// Verifica se il pronostico dell'utente è corretto
 if (pronostic === oddEven) {
-    alert("Vittoria");
+    alert("HAI VINTO !!!");
+} else {
+    alert("HAI PERSO ...");
 }
-else {
-    alert("Vgwergwgittoria");
-}
 
-alert(`Tu hai giocato ${userRoll}, il pc ha giocato ${computerRoll}, la somma è ${diceSum} che è ${oddEven}, e tu hai pronosticato ${pronostic}`);
-
-
-
-
-
-
-
-
-
-
+// Funzione per generare un numero casuale tra 1 e 6
 function randomDiceRoll() {
-  return Math.floor(Math.random() * 6) + 1;
+    return Math.floor(Math.random() * 6) + 1;
 }
 
+// Funzione per controllare se un numero è pari o dispari
 function checkEvenOdd(num) {
     if (num % 2 === 0) {
-        oddEven = "pari";
+        return "pari";
+    } else {
+        return "dispari";
     }
-    else {
-        oddEven = "dispari";
-    }
-    return oddEven;
 }
